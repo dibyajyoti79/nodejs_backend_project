2eef4d7979eefd01069e74559f8c982f4250afb9
dibyajyoti79 <dibyajyoti274@gmail.coom>
2023-12-04 11:33:02 +0530

    fix: bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import User from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -32,9 +32,18 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
-
+  // console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
 
-export { asyncHandler };
+// export { asyncHandler };
 
 //-==================================== using try catch ====================================-//
 
@@ -12,14 +12,14 @@ export { asyncHandler };
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
 
-// const asyncHandler = (requestHandler) => async (req, res, next) => {
-//   try {
-//     await requestHandler(req, res, next);
-//   } catch (error) {
-//     res.status(error.code || 500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
-// export { asyncHandler };
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    await requestHandler(req, res, next);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export { asyncHandler };
